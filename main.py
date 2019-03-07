927a9e6d616aa4be394ea2a0c0afc236f3be0890
Orel <orel0205@gmail.com>
2019-03-07 18:09:14 -0500

    Added Comment Sorting into arrays

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import praw
-r = praw.Reddit('bot1', user_agent="AskReddit Scraper .1 by Orel", )
+
+
+def comment_array(sub):
+    comment_arr = []
+    top_level_comments = list(sub.comments)[:25]
+    for comment in top_level_comments:
+        comment_arr.append(comment.body)
+    return comment_arr
+
+    
+r = praw.Reddit('bot1', user_agent="AskReddit Scraper .2 by Orel")
+sub_array = []
 for submission in r.subreddit('askreddit').top('day', limit=10):
     print(submission.title)
-    top_level_comments = list(submission.comments)[:10]
-    for comment in top_level_comments:
-        print(comment.body)
+    sub_array.append([submission.title, comment_array(submission)])
+    
+
