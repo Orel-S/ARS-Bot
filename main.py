722bdeec3cfaca39e89bd15887d57c50698e367c
Orel <orel0205@gmail.com>
2019-03-07 19:53:55 -0500

    added some text-to-speech functionality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import praw
+from gtts import gTTS
+import re
 
 
 def comment_array(sub):
@@ -8,11 +10,18 @@ def comment_array(sub):
         comment_arr.append(comment.body)
     return comment_arr
 
-    
+
+language = 'en'
 r = praw.Reddit('bot1', user_agent="AskReddit Scraper .2 by Orel")
 sub_array = []
 for submission in r.subreddit('askreddit').top('day', limit=10):
     print(submission.title)
+    title_string = re.sub('\'|\?|\.|\!|\/|\;|\:|\*', '', submission.title)
+    print(title_string)
+    g = gTTS(text=title_string, lang='en', slow=False)
+    g.save('Recordings//' + title_string + '.mp3')
     sub_array.append([submission.title, comment_array(submission)])
+
+
     
 
