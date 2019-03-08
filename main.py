83ae66a7970f985e6fee15b4a433a74a9d0efcec
Orel <orel0205@gmail.com>
2019-03-07 22:19:10 -0500

    fixed TTS, added comment TTS and folders

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import praw
 from gtts import gTTS
 import re
+import os
 
 
 def comment_array(sub):
@@ -14,14 +15,22 @@ def comment_array(sub):
 language = 'en'
 r = praw.Reddit('bot1', user_agent="AskReddit Scraper .2 by Orel")
 sub_array = []
-for submission in r.subreddit('askreddit').top('day', limit=10):
+for submission in r.subreddit('askreddit').top('month', limit=10):
     print(submission.title)
     title_string = re.sub('\'|\?|\.|\!|\/|\;|\:|\*', '', submission.title)
-    print(title_string)
-    g = gTTS(text=title_string, lang='en', slow=False)
-    g.save('Recordings//' + title_string + '.mp3')
-    sub_array.append([submission.title, comment_array(submission)])
-
+    sub_array.append([title_string, comment_array(submission)])
+    for item in sub_array:
+        shortened_title = re.sub('\'|\?|\.|\!|\/|\;|\:|\*', '', item[0])[ 0 : 0 + 15].rstrip()
+        if not os.path.exists("C:\\Users\\Orel\\PycharmProjects\\askredditbot\\Recordings\\" + shortened_title):
+            os.makedirs("C:\\Users\\Orel\\PycharmProjects\\askredditbot\\Recordings\\" + shortened_title)
+        g = gTTS(text=item[0], lang='en', slow=False)
+        g.save('Recordings\\' + shortened_title + '\\' + shortened_title + '.mp3')
+        for comment in item[1]:
+            shortened_comment = re.sub('\'|\?|\.|\!|\/|\;|\:|\*|\|\\n|\"', '', comment)[ 0 : 0 + 15]
+            shortened_comment = (shortened_comment.replace("\n", ' ')).rstrip()
+            g = gTTS(text=comment, lang='en', slow=False)
+            if shortened_comment != '':
+                g.save('Recordings\\' + shortened_title + '\\' + shortened_comment + '.mp3')
 
     
 
