1b6699cd68f072f79867e16af0f71cfefee1ef81
Orel S <shikher.orel@gmail.com>
2019-03-14 13:58:08 -0400

    Fixed bug with titles that contain quotation marks, made directories consistent

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import time
 
 init_time = time.time()
 LANGUAGE = 'en'
-POST_LIMIT = 10
-COMMENT_LIMIT = 20
-DIRECTORY = "C:\\Users\\Orel\\PycharmProjects\\askredditbot\\Recordings\\"
+POST_LIMIT = 2
+COMMENT_LIMIT = 5
+DIRECTORY = "C:\\Users\\orel0\\PycharmProjects\\askredditbot\\Recordings\\"
 FILE_EXTENSION = '.mp3'
 TIME = 'day'
 
@@ -27,19 +27,19 @@ sub_array = []
 for submission in r.subreddit('askreddit').top(TIME, limit=POST_LIMIT):
     print("Inserting the following submission into sub_array: ")
     print(submission.title)
-    title_string = re.sub('\'|\?|\.|\!|\/|\;|\:|\*', '', submission.title)
+    title_string = re.sub('\'|\?|\.|\!|\/|\;|\:|\*|\"', '', submission.title)
     sub_array.append([title_string, comment_array(submission)])
 # Second loop iterates through sub_array and creates sorted mp3 files
 j = 1
 audio_conversion_init_time = time.time()
 for item in sub_array:
     item_start_time = time.time()
-    shortened_title = re.sub('\'|\?|\.|\!|\/|\;|\:|\*', '', item[0])[ 0 : 0 + 15].rstrip()
+    shortened_title = re.sub('\'|\?|\.|\!|\/|\;|\:|\*|\"', '', item[0])[ 0 : 0 + 15].rstrip()
     print("Submission " + str(j) + "/" + str(POST_LIMIT) + " in progress...")
     if not os.path.exists(DIRECTORY + shortened_title):
         os.makedirs(DIRECTORY + shortened_title)
     g = gTTS(text=item[0], lang=LANGUAGE, slow=False)
-    g.save('Recordings\\' + shortened_title + '\\' + shortened_title + FILE_EXTENSION)
+    g.save(DIRECTORY + shortened_title + '\\' + shortened_title + FILE_EXTENSION)
     k = 1
     for comment in item[1]:
         print("Comment " + str(k) + "/" + str(COMMENT_LIMIT) + " in progress...")
@@ -47,7 +47,7 @@ for item in sub_array:
         shortened_comment = (shortened_comment.replace("\n", ' ')).rstrip()
         g = gTTS(text=comment, lang=LANGUAGE, slow=False)
         if shortened_comment != '':
-            g.save('Recordings\\' + shortened_title + '\\' + shortened_comment + FILE_EXTENSION)
+            g.save(DIRECTORY + shortened_title + '\\' + shortened_comment + FILE_EXTENSION)
         k += 1
     print("Elapsed time for submission " + str(j) + ": " + str((time.time() - item_start_time)) + " seconds.")
     j += 1
